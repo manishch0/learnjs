f0d165f825a897d8d919ce82d1a2f748a8b4181c
manish chaudhary <manishchamazon@gmail.com>
2023-03-01 10:17:43 +0530

    2 task

---
+++ AnsThe.js
@@ -0,0 +1,97 @@
+// Q1 and solution
+
+let num1 = 0;
+if (num1 === 0) {
+  console.log(true);
+} else console.log(false);
+
+// Q2 and solution
+
+// let number = 54;
+let number = 34;
+if (number !== 34) {
+  console.log(true);
+} else console.log(false);
+
+// Q3 and solution
+// let num = 5;
+let num = 1;
+if (num > 2) {
+  console.log("number is greater then 2");
+} else console.log("number is less then 2");
+
+// Q4 and solution
+
+let nums = 7;
+if (nums >= 7) {
+  console.log("number is greater pr equal to number 7");
+} else console.log("number is greater or less but not equal to num 7");
+
+// Q5 amd solution
+
+let numss = 10;
+if (num < 12) {
+  console.log("number is less the 12");
+} else console.log("number is greater then 12");
+
+// Q6 and solution
+
+let nums1 = 67;
+if ((num = 67)) {
+  console.log("number 67 is equal");
+} else console.log("number not equal to 67");
+
+// The Beginners Q and ans in js ex 2nd
+
+// Q7 and ans
+
+let a = 12;
+let b = 15;
+let c = 21;
+if (a > b && a > c) {
+  console.log("A is big number");
+} else if (b > a && b > c) {
+  console.log("B is big number");
+} else console.log("C is big no");
+
+// Q8 and ans
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 != 0) {
+    console.log(i);
+    console.log("data");
+  }
+}
+
+// Q9 and ans
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log(i++);
+  }
+}
+
+// Q10 and ans
+
+for (let i = 100; i >= 0; i--) {
+  console.log(i);
+}
+
+// Q11
+
+let set1 = 54;
+let set2 = 46;
+let ctotal = set1 + set2;
+if ((ctotal = 100)) {
+  console.log(true);
+} else console.log(false);
+
+// Q12
+
+let r1 = 4;
+let r2 = 5;
+let total = r1 + r2;
+
+if (total % 3 == 0) {
+  console.log(true);
+} else console.log(false);
